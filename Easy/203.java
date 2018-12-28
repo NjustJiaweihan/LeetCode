a1e2c598b12f690d3fa2853b1b23f304c9a0ea3b
Han J W <30855535+NjustJiaweihan@users.noreply.github.com>
2018-12-28 21:09:52 +0800

    Update 203.java

--- Easy/203.java
+++ Easy/203.java
@@ -22,3 +22,33 @@ class Solution
         return prehead.next;
     }
 }
+
+class Solution2
+{
+    public ListNode removeElements(ListNode head, int val)
+    {
+        //构造一个前驱结点 测试用例{6, 3, 2, ...}删除6
+        ListNode prehead = new ListNode(0);
+        prehead.next = head;
+        ListNode listNode = prehead;
+
+        while (listNode.next!=null)
+        {
+            //下面用while而不是if的初衷是连续删除连续指定结点
+            while (listNode.next.val == val)
+            {
+                listNode.next = listNode.next.next;
+                //5->null listNode.next = null
+                //进入while(listNode.next.val == val)判断 null.val无指向
+                //所以要加上下面的if
+                if(listNode.next == null)
+                {
+                    return prehead.next;
+                }
+            }
+            listNode = listNode.next;
+        }
+        //不返回head 测试用例{1} 删除1 返回null
+        return prehead.next;
+    }
+}
