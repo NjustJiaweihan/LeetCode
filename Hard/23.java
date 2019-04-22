1a601e6ace59ded36be3d077434a53a0350dfc96
Han J W <30855535+NjustJiaweihan@users.noreply.github.com>
2019-04-22 20:27:02 +0800

    Update 23.java

--- Hard/23.java
+++ Hard/23.java
@@ -40,3 +40,75 @@ class Solution
         return head.next;
     }
 }
+
+//归并排序
+
+class Solution
+{
+    public ListNode mergeKLists(ListNode[] lists)
+    {
+        if(lists == null || lists.length == 0)
+        {
+            return null;
+        }
+
+        int lo = 0, hi = lists.length-1;
+        while (lo < hi)
+        {
+            int mid = (hi + lo - 1) / 2;
+            for(int i=0; i<=mid; i++)
+            {
+                lists[i] = merge2Lists(lists[i], lists[hi - i]);
+            }
+            hi = (lo + hi) / 2;
+        }
+        return lists[0];
+    }
+
+    public ListNode merge2Lists(ListNode l1, ListNode l2)
+    {
+        if(l1 == null && l2 == null)
+        {
+            return null;
+        }
+        ListNode first = new ListNode(0);
+        ListNode last = first;
+
+        while (l1 != null || l2 != null)
+        {
+            if(l1 == null)
+            {
+                ListNode oldlast = last;
+                last = l2;
+                oldlast.next = last;
+
+                l2 = l2.next;
+            }
+            else if(l2 == null)
+            {
+                ListNode oldlast = last;
+                last = l1;
+                oldlast.next = last;
+
+                l1 = l1.next;
+            }
+            else if(l1.val > l2.val)
+            {
+                ListNode oldlast = last;
+                last = l2;
+                oldlast.next = last;
+
+                l2 = l2.next;
+            }
+            else
+            {
+                ListNode oldlast = last;
+                last = l1;
+                oldlast.next = last;
+
+                l1 = l1.next;
+            }
+        }
+        return first.next;
+    }
+}
