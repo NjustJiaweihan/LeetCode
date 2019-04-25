eb1211739d1fba48a54da10650601fb7b46ccd0c
Han J W <30855535+NjustJiaweihan@users.noreply.github.com>
2019-04-25 10:16:53 +0800

    Create 104.java

---
+++ Easy/104.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public int maxDepth(TreeNode root) 
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        return left > right ? left + 1 : right + 1;
+    }
+}
