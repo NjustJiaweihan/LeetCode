faa494cdf6a037e864cce91ae823a474aa69f8b5
Han J W <30855535+NjustJiaweihan@users.noreply.github.com>
2019-05-17 22:12:39 +0800

    Update 884.java

--- Easy/884.java
+++ Easy/884.java
@@ -66,3 +66,23 @@ class Solution {
         return result;
     }
 }
+
+
+//贴一个老哥的优雅代码
+class Solution {
+    public String[] uncommonFromSentences(String A, String B) {
+        Map<String, Integer> count = new HashMap();
+        for (String word: A.split(" "))
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        for (String word: B.split(" "))
+            count.put(word, count.getOrDefault(word, 0) + 1);
+
+        List<String> ans = new LinkedList();
+        for (String word: count.keySet())
+            if (count.get(word) == 1)
+                ans.add(word);
+
+        return ans.toArray(new String[ans.size()]);
+    }
+}
+
